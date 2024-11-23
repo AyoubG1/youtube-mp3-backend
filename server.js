ad383d76711cd15236269dc82a3639565d2d2795
AyoubG1 <ayoubdebbabi9@gmail.com>
2024-11-23 14:27:03 +0100

    Fix port binding for Render

--- server.js
+++ server.js
@@ -90,7 +90,10 @@ app.post('/download', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
+
 
